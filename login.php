f7d7fbf6b98db9983aa41d99e354490a9df4d2ba
MWEHEHEHE <cindy18ti@mahasiswa.pcr.ac.id>
2019-07-09 15:41:22 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sehat dimulai dari sini</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+ 
+	<h1>BMI </h1>
+ 
+	<div class="kotak_login">
+		<p class="tulisan_login">Silahkan login</p>
+ 
+		<form>
+			<label>Username</label>
+			<input type="text" name="username" class="form_login" placeholder="Username atau email ..">
+ 
+			<label>Password</label>
+			<input type="text" name="password" class="form_login" placeholder="Password ..">
+ 
+			<input type="submit" class="tombol_login" value="LOGIN">
+ 
+			<br/>
+			<br/>
+			<center>
+				<a class="link" href="">kembali</a>
+			</center>
+		</form>
+		
+	</div>
+ 
+</body>
+</html>
