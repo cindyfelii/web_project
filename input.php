2b05c166ac03869b701adaae161fa883e25bab92
Fitrins18 <fitri18ti@mahasiswa.pcr.ac.id>
2019-07-16 05:25:35 +0700

    Create input.php

---
+++ input.php
@@ -0,0 +1,19 @@
+
+
+<?php
+include"koneksi.php";
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$query = "insert INTO user SET
+								username = '$username',
+								password = '$password'
+								";
+
+mysqli_query($koneksi, $query)
+or die ("Gagal Perintah SQL".mysql_error());
+header('location:login.php');
+
+?>
+
